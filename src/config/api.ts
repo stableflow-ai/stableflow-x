6e0fdc4b49f72a33eb697a56a07042e71299d0aa
jimmygu <jimmygujh@gmail.com>
2026-07-28 19:11:24 +0800

    refactor: update development script and improve wallet component layout
    - Changed the 'dev' script in package.json to use 'vite --mode development'.
    - Updated IS_PRODUCTION constant in api.ts to use import.meta.env.PROD.
    - Enhanced the wallet component layout for better responsiveness and organization, including adjustments to the button placement and added EVM chain icons display.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "https://api.stableflow.ai";
-export const IS_PRODUCTION = import.meta.env.VITE_BASE_API_URL === "https://api.stableflow.ai";
+export const IS_PRODUCTION = import.meta.env.PROD;
 export const DB3_API_URL = "https://api.db3.app/api";
 
 export const RHEA_API_URL = "https://api.rhea.finance";
